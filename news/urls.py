654bec98b06f0fd31abdd0bc232f4ad1f9a8ea01
mai04031963 <136967136+mai04031963@users.noreply.github.com>
2023-07-06 18:31:35 +0300

    Add files via upload

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.news, name="news")
+    #path("", views.news, name="news")
+    path("", views.NewsListView.as_view(), name="news")
     ]
