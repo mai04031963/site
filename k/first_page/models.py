ef2940c43718d8b0ec1fbaefc1d76f4a103bf468
mai04031963 <136967136+mai04031963@users.noreply.github.com>
2023-07-10 15:25:21 +0300

    Delete k directory

--- k/first_page/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# Create your models here.
-
-
-class Firm(models.Model):
-    firm_name = models.CharField(max_length=40)
-    firm_adress = models.CharField(max_length=255)
-    firm_email = models.CharField(max_length=100)
-    firm_tel = models.CharField(max_length=20)
