afa61f91fbf3e80ab485af7bc2de694c48ff67a3
mai04031963 <136967136+mai04031963@users.noreply.github.com>
2023-07-10 15:26:52 +0300

    Delete news directory

--- news/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from datetime import date
-# Create your models here
-
-
-class News(models.Model):
-    news_date = models.DateField(auto_now_add=True)
-    news_text = models.TextField(blank=True)
